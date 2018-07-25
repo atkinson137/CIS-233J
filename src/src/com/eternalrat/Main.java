a2c11bbb4081a4c6e1b58edd8312cb5e228dbc3e
Rat <atkinson21@gmail.com>
2018-07-24 22:28:56 -0700

    Added print statements

--- src/src/com/eternalrat/Main.java
+++ src/src/com/eternalrat/Main.java
@@ -3,6 +3,7 @@ package com.eternalrat;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    System.out.println("Hello world!");
+	    System.out.println("Project by: Ryan Atkinson");
     }
 }
